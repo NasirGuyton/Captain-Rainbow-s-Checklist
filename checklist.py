fcc4cd8d0ba2fe21d45c1d03aabd57acb1ab5781
Student Guest <studentguest@Pauls-MacBook-Air.local>
2024-10-06 17:18:47 -0700

    created helper functions

--- checklist.py
+++ checklist.py
@@ -14,6 +14,8 @@ print(checklist)
 def create(item):
     checklist.append('Blue')
 
+checklist[0]
+
 # CREATE
 def create(item):
     checklist.append(item)
@@ -27,26 +29,89 @@ def read(index):
 def read(index):
     return checklist[index]
 
+checklist = ['Blue', 'Orange']
+checklist[1] = 'Cats'
+print (checklist)
+
 # UPDATE
 def update(index, item):
     checklist[index] = item
+
+checklist = ['Blue', 'Cats']
+checklist.pop(1)
+print(checklist)
 
 # DESTROY
 def destroy(index):
     checklist.pop(index)
 
 def test():
- create("purple sox")
-create("red cloak")
+    create("purple sox")
+    create("red cloak")
 
-print(read(0))
-print(read(1))
+    print(read(0))
+    print(read(1))
 
-update(0, "purple socks")
-destroy(1)
+    update(0, "purple socks")
 
-print(read(0))
-print(read(1))
+    destroy(1)
 
-test() 
+    print(read(0))
+
+    list_all_items()
+
+
+for list_item in checklist:
+    #Do something
+
+    def list_all_items():
+        for list_item in checklist:
+            print(list_item)
+def list_all_items():
+    index = 0
+    for list_item in checklist:
+        print(str(index) + list_item)
+        index += 1
+
+def mark_completed(index):
+    if 0 <= index < len(checklist):
+        checklist[index] = "√ " + checklist[index]
+    else:
+        print("Wrong")
+
+def select(function_code):
+    # Create item
+    if function_code == "C":
+        input_item = user_input("Input item:")
+        create(input_item)
+
+    # Read item
+    elif function_code == "R":
+        item_index = user_input("Index Number?")
+
+        # Remember that item_index must actually exist or our program will crash.
+        read(item_index)
+
+    # Print all items
+    elif function_code == "P":
+        list_all_items()
+
+    # Catch all
+    else:
+        print("Unknown Option")
+
+def user_input(prompt):
+    # the input function will display a message in the terminal
+    # and wait for user input.
+    user_input = input(prompt)
+    return user_input    
+def user_input(prompt):
+    # the input function will display a message in the terminal
+    # and wait for user input.
+    user_input = input(prompt)
+    return user_input
+
+ 
+
+
 
